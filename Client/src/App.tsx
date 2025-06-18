e18c91962bf93abaa56dd31b29410de70547950a
Nicholas Lopez <nico2k4@gmail.com>
2025-06-17 21:32:22 -0700

    successfully ported backend and frontend, sort of. We will see what is needed to be changed tomorrow

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,33 +1,61 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+const PATH = "http://localhost:8000/api";
+
 import './App.css'
 
+
+
 function App() {
-  const [count, setCount] = useState(0)
+  //const [count, setCount] = useState(0)
+  const [inputText, setInputText] = useState<string>('')
+  const [outputText, setOutputText] = useState<string>('')
+
+
+  async function fetchSearched(){
+      const response = await fetch(PATH.concat(`/${inputText}`))
+      console.log(`Here is the respone we got from our call ${response.status}`)
+      if (response.ok && inputText){
+        const msg = await response.json();
+        const fullMsg = JSON.stringify(msg.text)
+        console.log(fullMsg)
+        setOutputText(fullMsg)
+      }
+      else if(response.ok && !inputText){
+        const msg = await response.json()
+        console.log(msg)
+        //const jsonMsg = JSON.parse(msg)
+        let fullMsg = ""
+
+        msg.forEach((item: {id:number, text:string}) => {
+          fullMsg += ` ${item.text}\n`
+        });
+
+        setOutputText(fullMsg)
+      }
+      else{
+        setOutputText("Failed to recieve a response, check input")
+      }
+  }
+
+  
+
+
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        <input 
+          placeholder="Type in a ID number"
+          type="text" 
+          value={inputText} 
+          onChange={(e) => setInputText(e.target.value)}>
+        </input>
+
+        <button 
+          onClick={() => fetchSearched()}>
+          click me!
+          
+        </button>    
+        <p>From Backend {outputText} </p>
     </>
   )
 }
