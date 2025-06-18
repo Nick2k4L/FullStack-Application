2a94cb2f363a10c63960d09b8d1a12f2ed0aa50f
Nicholas Lopez <nico2k4@gmail.com>
2025-06-17 17:29:12 -0700

    Added a script so I do not need to constantly close and refresh the page. Also Working with server stuff and some requests

---
+++ Server/server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+
+const app = express();
+
+let message = [
+    {id: 1, text: 'Hello World!'},
+    {id: 2, text: 'Hello from the server!'},
+    {id: 3, text: 'Hello Everyone!'}
+]
+
+// here we are saying to get all posts
+app.get('/api', (req, res) => {
+  res.send(message);
+  console.log(req.body);
+})
+
+// here we are saying to get a post by an ID
+// So, if we go to localhost:8000/api/1, 
+// it will return the first post which is Hello World!
+app.get('/api/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const msg = message.find((msg) => msg.id === id);
+
+    if(msg){
+        return res.status(200).json(msg)
+    }
+
+    res.status(404).json({text: "This id does not exist"})
+    
+});
+
+app.listen(8000, () => console.log('Server is running on port 8000'));
