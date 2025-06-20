789ebbfa211a4d720bc98f7e50dbacf60c0183a0
Nicholas Lopez <nico2k4@gmail.com>
2025-06-20 15:51:51 -0700

    added a thunk to the project for fetching data

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,5 +1,4 @@
 import { useState } from 'react'
-const PATH = "http://localhost:8000/api";
 
 import './App.css'
 import Counter from './components/Counter';
@@ -7,42 +6,43 @@ import MyButton from './components/MyButton';
 import { useSelector } from 'react-redux';
 import type { RootState } from './state/store';
 import { useDispatch } from 'react-redux';
-import { setString } from './state/slices/stringSlice';
+import type { AppDispatch } from './state/store';
+import { stringAsync } from './state/slices/stringSlice';
 
 
 function App() {
   //const [count, setCount] = useState(0)
   const [inputText, setInputText] = useState<string>('')
-  //const [outputText, setOutputText] = useState<string>('')
   const output = useSelector((state: RootState) => state.string.value)
-  const dispatch = useDispatch()
+  const dispatch: AppDispatch = useDispatch()
+  //const dispatch = useDispatch()
 
-  async function fetchSearched(){
-      const response = await fetch(PATH.concat(`/${inputText}`))
-      //const response = await fetch(PATH)
-      console.log(`Here is the respone we got from our call ${response.status}`)
-      if (response.ok && inputText){
-        const msg = await response.json();
-        const fullMsg = JSON.stringify(msg.text)
-        console.log(fullMsg)
-        dispatch(setString(fullMsg))
-      }
-      else if(response.ok && !inputText){
-        const msg = await response.json()
-        console.log(msg)
-        //const jsonMsg = JSON.parse(msg)
-        let fullMsg = ""
+  // async function fetchSearched(){
+  //     const response = await fetch(PATH.concat(`/${inputText}`))
+  //     //const response = await fetch(PATH)
+  //     console.log(`Here is the respone we got from our call ${response.status}`)
+  //     if (response.ok && inputText){
+  //       const msg = await response.json();
+  //       const fullMsg = JSON.stringify(msg.text)
+  //       console.log(fullMsg)
+  //       dispatch(setString(fullMsg))
+  //     }
+  //     else if(response.ok && !inputText){
+  //       const msg = await response.json()
+  //       console.log(msg)
+  //       //const jsonMsg = JSON.parse(msg)
+  //       let fullMsg = ""
 
-        msg.forEach((item: {id:number, text:string}) => {
-          fullMsg += ` ${item.text}\n`
-        });
-        dispatch(setString(fullMsg))
+  //       msg.forEach((item: {id:number, text:string}) => {
+  //         fullMsg += ` ${item.text}\n`
+  //       });
+  //       dispatch(setString(fullMsg))
 
-      }
-      else{
-        dispatch(setString("Failed to recieve a response, check input"))
-      }
-  }
+  //     }
+  //     else{
+  //       dispatch(setString("Failed to recieve a response, check input"))
+  //     }
+  // }
 
   
 
@@ -64,7 +64,7 @@ function App() {
 
         <MyButton
           name="click me!"
-          onClick={() => fetchSearched()}
+          onClick={() => dispatch(stringAsync(inputText))}
         ></MyButton>
            
         <p>From Backend {output} </p>
--- Client/src/state/slices/stringSlice.ts
+++ Client/src/state/slices/stringSlice.ts
@@ -1,5 +1,6 @@
 import type { PayloadAction } from "@reduxjs/toolkit"
-import { createSlice } from "@reduxjs/toolkit"
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit"
+const PATH = "http://localhost:8000/api";
 
 interface StringState{
     value: string
@@ -17,7 +18,47 @@ const stringSlice = createSlice({
             state.value = action.payload
         }
     },
+    extraReducers: (builder) => {
+
+        // if it is okay / fulfilled then its okay. 
+        // There is also a rejected case for when there is a rejection
+        builder.addCase(stringAsync.fulfilled, (state, action: PayloadAction<string>) => {
+            state.value = action.payload
+        })
+        
+    },
 })
+
+export const stringAsync = createAsyncThunk(
+    "string/stringAsync",
+    async (inputText: string) => {
+        const response = await fetch(PATH.concat(`/${inputText}`))
+        console.log(`Here is the respone we got from our call ${response.status}`)
+
+      if (response.ok && inputText){
+        const msg = await response.json();
+        const fullMsg = JSON.stringify(msg.text)
+        console.log(fullMsg)
+        return fullMsg
+      }
+      else if(response.ok && !inputText){
+        const msg = await response.json()
+        console.log(msg)
+        //const jsonMsg = JSON.parse(msg)
+        let fullMsg = ""
+
+        msg.forEach((item: {id:number, text:string}) => {
+          fullMsg += ` ${item.text}\n`
+        });
+
+        return fullMsg
+
+      }
+      else{
+        return ("Failed to recieve a response, check input")
+      }
+    }
+)
 
 export const { setString } = stringSlice.actions
 export default stringSlice.reducer
